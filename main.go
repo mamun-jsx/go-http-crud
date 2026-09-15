e3f776e1a0e41ff7f5cc423c165eb2ee3f6671a5
almamun528 <ayapths@gmail.com>
2026-09-16 03:21:29 +0600

    adding content type into response header

--- main.go
+++ main.go
@@ -1,9 +1,24 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
+
+type User struct {
+	Id    int
+	Name  string
+	Age   int
+	Email string
+}
+
+var users = []User{
+	{Id: 1, Name: "Kamal", Age: 11, Email: "kk@gmail.com"},
+	{Id: 2, Name: "Jamal", Age: 32, Email: "jamal@gmail.com"},
+	{Id: 3, Name: "Ruhul", Age: 23, Email: "huhul@gmail.com"},
+	{Id: 4, Name: "Habib", Age: 41, Email: "habib@gmail.com"},
+}
 
 func main() {
 	mux := http.NewServeMux() // under the hood default mux will be used.
@@ -11,7 +26,8 @@ func main() {
 	mux.HandleFunc("/", rootHandler)
 	fmt.Println("server is running on 4000")
 	mux.HandleFunc("/health", healthHandler)
-	mux.HandleFunc("/create-user", createUserHandler)
+	mux.HandleFunc("POST /create-user", createUserHandler)
+	mux.HandleFunc("GET /users", getUserHandler)
 
 	// port listen
 	err := http.ListenAndServe(":4000", mux)
@@ -29,14 +45,16 @@ func healthHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "server is up and healthy")
 
 }
-func createUserHandler(w http.ResponseWriter, r *http.Request) {
 
-	if r.Method != "POST" {
-		w.WriteHeader(http.StatusMethodNotAllowed)
-		fmt.Fprintln(w, "Only post method is allowed")
-		return
-	}
+func createUserHandler(w http.ResponseWriter, r *http.Request) {
 
 	fmt.Fprintln(w, "user created")
 
+}
+
+func getUserHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "Application/json")
+	users, _ := json.Marshal(users)
+	// ! send data to client side
+	w.Write(users)
 }
